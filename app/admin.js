8adb099e842538ecb945b61808adb87e9008de72
Agos Mercado <agosmercado@gmail.com>
2022-11-10 22:25:10 -0300

    Pruebo footer en landing page

---
+++ app/admin.js
@@ -0,0 +1,18 @@
+// // PROTECCION DE RUTAS
+// let userLoggedLS = JSON.parse(localStorage.getItem("userLogged"));
+// if(!userLoggedLS){ //SI NO EXISTE USUARIO LOGUEADO REDIRIJO AL LOGIN
+//   window.location.asign(window.location.origin) + "/login.html";
+// }else if (!userLoggedLS.admin){ //SI EXISTE USUARIO LOGUEADO Y NO ES ADMIN REDIRIJO A PAGIN PRINCIPAL
+//   window.location.asign(window.location.origin) + "/pagprincipal.html";
+// }
+
+// class Peliculas{
+//   constructor(nombre, id, descripcion, imagen, destacado, publicado){
+//     this.nombre=nombre;
+//     this.id=id;
+//     this.descripcion=descripcion;
+//     this.imagen=imagen;
+//     this.destacado=destacado;
+//     this.publicado=publicado
+//   }
+// }
