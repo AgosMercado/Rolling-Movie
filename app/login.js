5bbc6e26e52b3100a162dddd832de1c805703e2c
Agos Mercado <agosmercado@gmail.com>
2022-11-19 13:20:17 -0300

    modal olvide mi contraseña

--- app/login.js
+++ app/login.js
@@ -59,3 +59,25 @@ const validationRegister = (email, password)=>{
     if(!passwordOk) errors.password = " Verifica la contraseña ingresada";
     return errors;
 }
+
+
+const validationRegister2 = (email2)=>{  
+    let errors = {};
+    let emailOk = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email2);
+    if(!emailOk) errors.email = " Verifica el email ingresado";
+    return errors;
+}
+const validationForgetPassword = (e)=>{
+    e.preventDefault()
+    let email2 = document.getElementById("password-email-value").value;
+    let errorsObject = validationRegister2(email2); //traigo el "objeto Errores" de la funcion
+    let errorsKeys = Object.keys(errorsObject);  //Traigo el array de propiedades del objeto Errores
+    let errorsValues = Object.values(errorsObject); //Traigo el array de valores del objeto errores
+    if (errorsKeys.length==0){
+        alertMessage (" ¡Listo! Te enviamos un email con instrucciones para recuperar tu contraseña","#container-alertMessage2");
+    }else{
+        errorsValues.map(error=>{
+            alertMessage (error,"#container-alertMessage2");
+        })
+    }
+}
